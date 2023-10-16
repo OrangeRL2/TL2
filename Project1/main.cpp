28ea9f9b96015187dcdc8874cc1d3e126e28b0a6
Jim <jimnauldee@gmail.com>
2023-10-16 15:03:40 +0900

    DirectXTex

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,12 @@
 #include "cstdio"
 #include "cstdlib"
 
-int main() {
+#include <DirectXTex.h>
+
+int main(int argc, char* argv[]) {
+	for (int i = 0; i < argc; i++) {
+
+	}
 	printf("Hello,World!\n");
 	system("pause");
 
